5e57f0f27301ce1c41d934cdf7f38ec115701aba
Carlos <carlosvergara0912@gmail.com>
2020-07-30 10:39:18 -0500

    se creo las migraciones y los modelos

---
+++ database/migrations/2020_07_30_151432_create_productos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sku')->unique();
+            $table->string('descripcion');
+            $table->string('valor');
+            $table->bigInteger('tienda')->unsigned();
+            $table->string('imagen');
+
+            $table->timestamps();
+            $table->foreign('tienda')->references('id')->on('tiendas')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
